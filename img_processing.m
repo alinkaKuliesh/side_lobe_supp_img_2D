8404a907bcda9b3dfe26ceed79ba3b1dbe4539ff
Alina Kuliesh <alina.kuliesh@gmail.com>
2021-09-15 13:10:52 +0200

    NC 4

--- img_processing.m
+++ img_processing.m
@@ -15,9 +15,9 @@ switch resolution
     case 0.5
         load('RESULTS/xWave.mat')
         line_start = 1;
-        line_finish = 11;
+        line_finish = 34;
         file_mb = 'RESULTS/xWave_disc.mat'; % mb = main beam
-        file_cb = 'RESULTS/xWave_inv_NC2.mat'; % cb = comp beam
+        file_cb = 'RESULTS/xWave_inv_disc.mat'; % cb = comp beam
         
     case 0.25
 %         load('RESULTS/xWave_lambda_pitch.mat')
@@ -36,7 +36,7 @@ end
 x_vec = x_vec(line_start:line_finish);
 
 % reconstruction of the image from double beam
-% [IQbf_inv, ~, ~] = RF2img(file_cb, 'verasonics', line_start, line_finish, resolution);
+[IQbf_inv, ~, ~] = RF2img(file_cb, 'verasonics', line_start, line_finish, resolution);
 
 %% image
 interpolate = 1; % interpolate in longitudinal direction
@@ -51,10 +51,10 @@ I(I < -DR) = -DR;
 Env_inv = abs(IQbf_inv); % real envelope
 if interpolate
     Env_inv = interp2(x_vec, z_vec', Env_inv, interpn(x_vec), z_vec');
-    x_vec = interpn(x_vec);
+%     x_vec = interpn(x_vec);
 end
 I_inv = 20*log10(Env_inv/max(Env_inv, [], 'all'));
-% I_inv(I_inv < -DR) = -DR;
+I_inv(I_inv < -DR) = -DR;
 
 figure()
 imagesc(x_vec, z_vec, I); hold on
@@ -74,8 +74,8 @@ pos_z_start = find(floorn(z_vec, 4) == 2.5e-3, 1);
 pos_z_end = find(floorn(z_vec, 4) == 6e-3, 1);
 
 figure()
-imagesc(x_vec*1e3, z_vec(pos_z_start:pos_z_end)*1e3, I(pos_z_start:pos_z_end, :)); 
-% imagesc(I(1:pos_z, :)); 
+% imagesc(x_vec*1e3, z_vec(pos_z_start:pos_z_end)*1e3, I(pos_z_start:pos_z_end, :)); 
+imagesc(I(pos_z_start:pos_z_end, :)); 
 axis image
 xlabel('x [mm]')
 ylabel('z [mm]')
@@ -152,15 +152,16 @@ colorbar
 colormap gray
 
 figure()
-imagesc(x_vec*1e3, z_vec(pos_z_start:pos_z_end)*1e3, I(pos_z_start:pos_z_end, :)); hold on
+% imagesc(x_vec*1e3, z_vec(pos_z_start:pos_z_end)*1e3, I(pos_z_start:pos_z_end, :)); hold on
+imagesc(I(pos_z_start:pos_z_end, :));
 axis image
 title('IQbf\_main')
 colorbar
 colormap gray
 
 %% cross-section line of the PSF
-[~, pos_z] = min(abs(z_vec-xv(end)-0.0128e-3));
-% pos_z = 304;
+% [~, pos_z] = min(abs(z_vec-xv(end)-0.0128e-3));
+pos_z = 248 + pos_z_start;
 
 % envelope [dB]
 figure()
@@ -173,8 +174,8 @@ legend
 
 figure()
 plot(x_vec*1e3, I(pos_z,:), 'DisplayName', 'E\_main'); hold on
-% plot(x_vec*1e3, I_diff(pos_z, :), 'DisplayName', 'norm(E\_main) - 0.3*norm(E\_comp)'); 
-plot(x_vec*1e3, I_diff(pos_z, :), 'DisplayName', 'E\_main - E\_comp(NC = 2)'); 
+plot(x_vec*1e3, I_diff(pos_z, :), 'DisplayName', 'norm(E\_main) - 0.3*norm(E\_comp)'); 
+% plot(x_vec*1e3, I_diff(pos_z, :), 'DisplayName', 'E\_main - E\_comp(NC = 2)'); 
 xlabel('longitudinal direction [mm]')
 ylabel('Envelope difference [dB]')
 title (strcat('PSF at z =  ', num2str(z_vec(pos_z)*1e3), ' mm'))
